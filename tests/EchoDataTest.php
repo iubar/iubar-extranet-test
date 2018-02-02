f3ee1ea3b4afc29dad05b3174993196e38a68dc8
Daniele <daniele.montesi@iubar.it>
2018-02-02 10:39:44 +0100

    .

--- tests/EchoDataTest.php
+++ tests/EchoDataTest.php
@@ -11,14 +11,14 @@ use Iubar\Tests\RestApi_TestCase;
  * @see: https://httpbin.org/
  */
 class EchoDataTest extends RestApi_TestCase {
-    
+
     const ECHO_ROUTE = 'echo';
-    
+
     public static function setUpBeforeClass() {
         parent::init();
-        self::$client = self::factoryClient();           
+        self::$client = self::factoryClient();
     }
-    
+
     // From Guzzle's doc:
     // Use 'form_params' for application/x-www-form-urlencoded requests,
     // and 'multipart' for multipart/form-data requests.
@@ -29,7 +29,7 @@ class EchoDataTest extends RestApi_TestCase {
     // As it is explained in W3, the content type "multipart/form-data" should be used for submitting forms that contain files, non-ASCII data, and binary data.
     //
     // POST requests in Guzzle are sent with an application/x-www-form-urlencoded Content-Type header if POST fields are present but no files are being sent in the POST. If files are specified in the POST request, then the Content-Type header will become multipart/form-data.
-    
+
     public function setUp() {
         // nothing to do
     }
@@ -48,12 +48,12 @@ class EchoDataTest extends RestApi_TestCase {
             ],
             'query' => $array
         ];
-                
+
         $response = self::$client->request(self::GET, self::ECHO_ROUTE, $data);
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString(json_encode($array), json_encode($data['data']));
     }
-    
+
     public function testEchoGet2() { // Send a GET request (using Psr7\Request) (encoding data with http_build_query())
         self::$climate->comment('Testing Echo->get(...');
         $array = array(
@@ -68,7 +68,7 @@ class EchoDataTest extends RestApi_TestCase {
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString(json_encode($array), json_encode($data['data']));
     }
-    
+
     public function testEchoGet3() { // Send a GET request (using Psr7\Request without encoding data)
         self::$climate->comment('Testing Echo->get(...');
         $array = array(
@@ -82,10 +82,10 @@ class EchoDataTest extends RestApi_TestCase {
         ]);
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString(json_encode($array), json_encode($data['data']));
-    }    
-    
+    }
+
     public function testEchoPost() { // Send an 'application/x-www-form-urlencoded' POST request (using Psr7\Request)
-        
+
         self::$climate->comment('Testing Echo->post(...');
         $array = array(
             'Foo' => 'Bar1'
@@ -96,33 +96,33 @@ class EchoDataTest extends RestApi_TestCase {
         $request = new Request(self::POST, self::ECHO_ROUTE, $headers, $encoded_data);
         $response = self::$client->send($request, [
             'timeout' => self::TIMEOUT
-        ]);        
-        $data = $this->checkResponse($response);        
+        ]);
+        $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString(json_encode($array), json_encode($data['data']));
     }
-    
+
     public function testEchoPost2() { // Post Json data
-        
+
         self::$climate->comment('Testing Echo->post(...');
         $array = array(
             'Foo' => 'Bar2'
-        );        
+        );
         $json = json_encode($array);
-        self::$climate->info('Request data: ' . $json);     
+        self::$climate->info('Request data: ' . $json);
         $response = self::$client->request(self::POST, self::ECHO_ROUTE, [
             'headers' => [
-                'Content-Type' => 'application/json; charset=UTF-8', 
+                'Content-Type' => 'application/json; charset=UTF-8',
                 'X-Requested-With' => 'XMLHttpRequest'
                 ],
-            'body' => $json 
+            'body' => $json
         ]);
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString($json, json_encode($data['data']));
     }
-   
-        
+
+
     public function testEchoPost3() { // Send an 'application/x-www-form-urlencoded' POST request (using request() method)
-        
+
         self::$climate->comment('Testing Echo->post(...');
         $array = array(
             'Foo' => 'Bar3'
@@ -133,58 +133,50 @@ class EchoDataTest extends RestApi_TestCase {
             'headers' => [
                 'Content-Type' => 'application/x-www-form-urlencoded', // obbligatorio quando si usa 'form_params'
                 'X-Requested-With' => 'XMLHttpRequest'
-            ],            
+            ],
            'form_params' => $array, // This option cannot be used with body, multipart, or json
 		   'connect_timeout' => self::TIMEOUT, 	// The number of seconds to wait while trying to connect to a server
 		   'timeout' => self::TIMEOUT 			// The timeout of the request in seconds
-		]);                       
+		]);
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString($json, json_encode($data['data']));
-    }    
-    
-    
-// MULTIPART 
+    }
+
+
+// MULTIPART
 
 
     public function testEchoPost6() { // Send a 'multipart/form-data' POST request
         self::$climate->comment('Testing Echo->post(...');
         $array = [
-       
+
             'headers'  => ['X-Requested-With' => 'XMLHttpRequest'],
             'multipart' => [
                 [
                     'name'     => 'John',
                     'contents' => 'Worker',
-                    
-                ],
-                 [
-                     'name'     => 'Bob',
-                     'contents' => fopen(__FILE__, 'r')
-                 ],
-                [
-                    'name'     => 'Alice',
-                    'contents' => fopen(__FILE__, 'r'),
-                    'filename' => 'custom_filename.txt'
-                ],
+
+                ]
             ]
         ];
-        
+
         $encoded_data = json_encode($array);
         self::$climate->info('Request data: ' . $encoded_data);
         $response = self::$client->request(self::POST, self::ECHO_ROUTE, $array);
         $data = $this->checkResponse($response);
-        $actual_value = $data['data']['files']['Alice']['name'];
-        $expected_value = $array['multipart']['2']['filename'];
+		$actual_value = $data['data']['John'];
+        $expected_value = $array['multipart'][0]['contents'];
+
         $this->assertEquals($expected_value, $actual_value);
-        
+
     }
-    
+
     /**
      * Metodo incompleto: restituisce error 'Fatal error: Class 'GuzzleHttp\Psr7\stream_for' not found'
      * @see http://guzzle.readthedocs.io/en/latest/psr7.html#streams
      */
     public function NON_VA_testEchoPost4() { // Send a 'multipart/form-data' POST request (using Psr7\Request)
-    
+
         self::$climate->comment('Testing Echo->post(...');
         $array = array(
             'name'     => 'field_name',
@@ -192,7 +184,7 @@ class EchoDataTest extends RestApi_TestCase {
         );
         $json = json_encode($array);
         self::$climate->info('Request data: ' . $json);
-    
+
         // $stream = GuzzleHttp\Psr7\stream_for('foo');
         $body = new Psr7\stream_for(http_build_query($array)); // http://guzzle.readthedocs.io/en/latest/psr7.html#streams
         $boundary = uniqid();
@@ -205,9 +197,9 @@ class EchoDataTest extends RestApi_TestCase {
         $data = $this->checkResponse($response);
         $this->assertJsonStringEqualsJsonString($json, json_encode($data['data']));
     }
-    
+
     // ALTRI ESEMPI
-        
+
     // Esempio di chiamata asincrona
     //
     // public function sendReqAsync(){
@@ -223,7 +215,7 @@ class EchoDataTest extends RestApi_TestCase {
     // oppure...
     // $request = new \GuzzleHttp\Psr7\Request('GET', $this->url);
     // $promise = $client->sendAsync($request, ['timeout' => 6, 'query' => $this->getQuery()])->then(
-    //          	
+    //
     // function (ResponseInterface $response) {
     // echo $response->getStatusCode() . PHP_EOL;
     // echo 'I completed! ' . $response->getBody()->getContents();
@@ -231,9 +223,9 @@ class EchoDataTest extends RestApi_TestCase {
     // function (RequestException $e) {
     // echo $e->getMessage() . PHP_EOL;
     // }
-    //            	
+    //
     // );
     // $promise->wait();
-    // }    
-    
+    // }
+
 }
